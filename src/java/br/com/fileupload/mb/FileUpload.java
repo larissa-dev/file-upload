f416672584085e2999c76da7897173822b69951e
Larissa Alves <larissa.alves.dev@gmail.com>
2020-05-20 20:27:06 -0300

    Index.xhtml

--- src/java/br/com/fileupload/mb/FileUpload.java
+++ src/java/br/com/fileupload/mb/FileUpload.java
@@ -4,13 +4,12 @@ package br.com.fileupload.mb;
 import javax.annotation.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
-package br.com.nooclix.primeupload.mb;
-
 
 import java.io.IOException;
 import java.util.Scanner;
 import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
+//import javax.faces.bean.ManagedBean;
+//import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
@@ -23,23 +22,23 @@ import org.primefaces.model.UploadedFile;
  * @author Larissa Alves
  */
 
-@ManagedBean(name="arquivoUpload")
+@ManagedBean
 @ViewScoped
 public class FileUpload {
     private UploadedFile arquivo;
     public String conteudo;
 
-        public String getConteudoDoArquivo() {
+        public String getConteudo() {
             return conteudo;
         }
 
-        public void setConteudoDoArquivo(String conteudo) {
+        public void setConteudo(String conteudo) {
             this.conteudo = conteudo;
         }    
    
      
     public void handleFileUpload(FileUploadEvent event) throws IOException {
-        FacesMessage msg = new FacesMessage("Arquivo adicionado com sucesso", event.getFile().getFileName() + " is uploaded.");
+        FacesMessage msg = new FacesMessage("Sucesso", event.getFile().getFileName() + " is uploaded.");
      
         FacesContext.getCurrentInstance().addMessage(null, msg);
         
